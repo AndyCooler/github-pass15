83a1c03b09e194c1a96970d55c6bf85713e7df6d
Andreas Kohler <andreas.kohler.76@googlemail.com>
2018-07-10 23:50:36 +0200

    added first version of copy-to-clipboard feature

--- app/src/main/java/com/mythosapps/pass15/EditPasswordUI.java
+++ app/src/main/java/com/mythosapps/pass15/EditPasswordUI.java
@@ -1,9 +1,15 @@
 package com.mythosapps.pass15;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.text.InputType;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -85,7 +91,27 @@ public class EditPasswordUI {
         TextView usernameLabel = new TextView(parent);
         usernameLabel.setText(R.string.edit_password_username);
         linearLayout.addView(usernameLabel);
-        linearLayout.addView(usernameTextField);
+
+        LinearLayout userNamelinearLayout = new LinearLayout(parent);
+        userNamelinearLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        userNamelinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        userNamelinearLayout.addView(usernameTextField);
+        Button bt = new Button(parent);
+        bt.setGravity(1);
+        bt.setText("Copy");
+        bt.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClipboardManager clipboard = (ClipboardManager) parent.getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("user name", usernameTextField.getText());
+                clipboard.setPrimaryClip(clip);
+            }
+        });
+        userNamelinearLayout.addView(bt);
+        linearLayout.addView(userNamelinearLayout);
 
         passwordTextField = new EditText(parent);
         int mode = show ? InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL : InputType.TYPE_CLASS_TEXT| InputType.TYPE_TEXT_VARIATION_PASSWORD;
@@ -93,10 +119,31 @@ public class EditPasswordUI {
         passwordTextField.setText(existingEntry.getPassword());
         passwordTextField.setSelection(0, existingEntry.getPassword().length());
         passwordTextField.setEnabled(true);
+        passwordTextField.setGravity(4);
         TextView passwordLabel = new TextView(parent);
         passwordLabel.setText(R.string.edit_password_password);
         linearLayout.addView(passwordLabel);
-        linearLayout.addView(passwordTextField);
+
+        LinearLayout passwordlinearLayout = new LinearLayout(parent);
+        passwordlinearLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT));
+        passwordlinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+        passwordlinearLayout.addView(passwordTextField);
+        Button btPwd = new Button(parent);
+        btPwd.setGravity(1);
+        btPwd.setText("Copy");
+        btPwd.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        btPwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClipboardManager clipboard = (ClipboardManager) parent.getSystemService(Context.CLIPBOARD_SERVICE);
+                ClipData clip = ClipData.newPlainText("password", passwordTextField.getText());
+                clipboard.setPrimaryClip(clip);
+            }
+        });
+        passwordlinearLayout.addView(btPwd);
+        linearLayout.addView(passwordlinearLayout);
 
         builder.setView(linearLayout);
 
